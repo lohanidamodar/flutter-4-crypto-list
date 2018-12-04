410566a4eeda80196378c60e70f7bf2e7ca47e65
Damodar Lohani <dlohani48@gmail.com>
2018-12-04 13:19:13 +0545

    making http requests

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 
+void main() async {
+  List currencies = await getCurrencies();
+  print(currencies);
+  runApp(
+    new MaterialApp(
+      title: 'Crypto Diary',
+      home: new Center(
+        child: new Text('Hello World!')
+      )
+    )
+  );
+}
 
-void main() {
-  print('hello world');
+Future<List> getCurrencies() async {
+  String url = 'https://api.coinmarketcap.com/v1/ticker/?limit=50';
+  http.Response response = await http.get(url);
+  return json.decode(response.body);
 }
